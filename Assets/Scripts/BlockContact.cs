61256ad46e1ce7a452f702e7727199c49a5ffa92
TitaniumLab <156667078+TitaniumLab@users.noreply.github.com>
2024-03-14 20:54:30 +0300

    Base prototype.
    -implemented transition along internal corners.
    -Implemented jump with gravity change.

--- Assets/Scripts/BlockContact.cs
+++ Assets/Scripts/BlockContact.cs
@@ -1,17 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockContact : MonoBehaviour
 {
-    public bool isOnSurface;//if edge touches surface
+    public bool isOnSurface;//is edge touches surface
+    private List<GameObject> surfaces = new List<GameObject>();
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Surface")
+        {
+            surfaces.Add(collision.gameObject);
             isOnSurface = true;
+        }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Surface")
+        if (collision.collider.tag == "Surface" && surfaces.Count < 2)
             isOnSurface = false;
+        surfaces.Remove(collision.gameObject);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private BlockContact bottomLeftBlock;//left bottom part of cube
-    [SerializeField]
-    private BlockContact bottomRightBlock;//right bottom part of cube
+    [SerializeField] private BlockContact bottomLeftBlock;//left bottom part of cube
+    [SerializeField] private BlockContact bottomRightBlock;//right bottom part of cube
+    [SerializeField] private BlockContact upperLeftBlock;//left upper part of cube
+    [SerializeField] private BlockContact upperRightBlock;//right upper part of cube
     private Rigidbody2D playerRigidbody;
-    private float horizontalInput;//value for movement
-    private float defaultGravity = 20;//default gravity value
+    [SerializeField] private float horizontalInput;//value for movement
+    private float defaultGravity = 25;//default gravity value
     private Animation rotateAnimation; //controls rotation animation
-    private bool canRotateAgain = true; // prevents glitch after rotarion
+    [SerializeField] private bool canRotateAgain = true; // prevents glitch after rotarion
+    [SerializeField] private bool canJumpAgain = true; // prevents glitch after jump
+    [SerializeField] private bool isRotating = false; //
+    [SerializeField] private bool jumpAfterRorating = false;
+
 
     private enum State //all posible directions
     {
@@ -32,19 +36,42 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //movement
         horizontalInput = Input.GetAxis("Horizontal");
-        playerRigidbody.AddRelativeForce(Vector2.right * horizontalInput * Time.deltaTime * 1000);
+        if (bottomLeftBlock.isOnSurface && bottomRightBlock.isOnSurface && canJumpAgain)
+            playerRigidbody.AddRelativeForce(Vector2.right * horizontalInput * Time.deltaTime * 1000);
+        //default jump
+        if (Input.GetKeyDown(KeyCode.Space) && (bottomLeftBlock.isOnSurface && bottomRightBlock.isOnSurface) && !upperLeftBlock.isOnSurface && !upperRightBlock.isOnSurface)
+            Jump();
+        //if try jump during rotating
+        if (Input.GetKeyDown(KeyCode.Space) && isRotating)
+            jumpAfterRorating = true;
+
+    }
+
+    private void LateUpdate()
+    {
+        //if cube touches the ground with both edges
+        if (bottomLeftBlock.isOnSurface && bottomRightBlock.isOnSurface && !canRotateAgain && !upperLeftBlock.isOnSurface && !upperRightBlock.isOnSurface)
+            canRotateAgain = true;
+
+
     }
 
     private void FixedUpdate()
     {
-        // if one of the edges is hanging down, cube is not in the air and has not recently rotated
-        if ((!bottomLeftBlock.isOnSurface || !bottomRightBlock.isOnSurface) && !(!bottomLeftBlock.isOnSurface && !bottomRightBlock.isOnSurface) && canRotateAgain)
+        // if one of the edges is hanging down, the cube is not in the air, has not recently rotated or jumped
+        if ((!bottomLeftBlock.isOnSurface || !bottomRightBlock.isOnSurface) && !(!bottomLeftBlock.isOnSurface && !bottomRightBlock.isOnSurface) && canRotateAgain && canJumpAgain)
             ChangeDirection();
 
-        //if cube touches the ground with both edges
-        if (bottomLeftBlock.isOnSurface && bottomRightBlock.isOnSurface && !canRotateAgain)
-            canRotateAgain = true;
+        // if one of the top edges does not touch the surface, has not recently rotated or jumped
+        if ((!upperLeftBlock.isOnSurface || !upperRightBlock.isOnSurface) && !(!upperLeftBlock.isOnSurface && !upperRightBlock.isOnSurface) && canRotateAgain && canJumpAgain)
+            ChangeDirectionInternal();
+
+
+        //stop sliding after button up
+        if (horizontalInput == 0 && bottomLeftBlock.isOnSurface && bottomRightBlock.isOnSurface && (playerRigidbody.velocity.x != 0 || playerRigidbody.velocity.y != 0) && canJumpAgain)
+            playerRigidbody.velocity = Vector2.zero;
     }
 
 
@@ -53,19 +80,18 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void ChangeDirection()
     {
+        isRotating = true;
         playerRigidbody.velocity = Vector2.zero;
         if (bottomLeftBlock.isOnSurface && !bottomRightBlock.isOnSurface)
         {
             switch (state)
             {
                 case State.vectorUp:
-
                     Physics2D.gravity = Vector2.left * defaultGravity;
                     rotateAnimation.Play("1_UpToRightRotation");
                     state = State.vectorRight;
                     break;
                 case State.vectorRight:
-
                     Physics2D.gravity = Vector2.up * defaultGravity;
                     rotateAnimation.Play("2_RightToDownRotation");
                     state = State.vectorDown;
@@ -110,5 +136,133 @@ public class PlayerController : MonoBehaviour
             }
         }
         canRotateAgain = false;
+    }
+
+    /// <summary>
+    /// change cube rotarion and grabity on internal angle
+    /// </summary>
+    private void ChangeDirectionInternal()
+    {
+        isRotating = true;
+        if (upperRightBlock.isOnSurface)
+        {
+            switch (state)
+            {
+                case State.vectorUp:
+                    playerRigidbody.AddForce(Vector2.up * 4, ForceMode2D.Impulse);
+                    Physics2D.gravity = Vector2.right * defaultGravity;
+                    rotateAnimation.Play("5_UpToLeftRotation");
+                    state = State.vectorLeft;
+                    break;
+                case State.vectorLeft:
+                    playerRigidbody.AddForce(Vector2.left * 4, ForceMode2D.Impulse);
+                    Physics2D.gravity = Vector2.up * defaultGravity;
+                    rotateAnimation.Play("6_LeftToDownRotation");
+                    state = State.vectorDown;
+                    break;
+                case State.vectorDown:
+                    playerRigidbody.AddForce(Vector2.down * 4, ForceMode2D.Impulse);
+                    Physics2D.gravity = Vector2.left * defaultGravity;
+                    rotateAnimation.Play("7_DownToRightRotation");
+                    state = State.vectorRight;
+                    break;
+                case State.vectorRight:
+                    playerRigidbody.AddForce(Vector2.right * 4, ForceMode2D.Impulse);
+                    Physics2D.gravity = Vector2.down * defaultGravity;
+                    rotateAnimation.Play("8_RightToUpRotation");
+                    state = State.vectorUp;
+                    break;
+
+
+            }
+        }
+        else if (upperLeftBlock.isOnSurface)
+        {
+            switch (state)
+            {
+                case State.vectorUp:
+                    playerRigidbody.AddForce(Vector2.up * 4, ForceMode2D.Impulse);
+                    Physics2D.gravity = Vector2.left * defaultGravity;
+                    rotateAnimation.Play("1_UpToRightRotation");
+                    state = State.vectorRight;
+                    break;
+                case State.vectorRight:
+                    playerRigidbody.AddForce(Vector2.right * 4, ForceMode2D.Impulse);
+                    Physics2D.gravity = Vector2.up * defaultGravity;
+                    rotateAnimation.Play("2_RightToDownRotation");
+                    state = State.vectorDown;
+                    break;
+                case State.vectorDown:
+                    playerRigidbody.AddForce(Vector2.down * 4, ForceMode2D.Impulse);
+                    Physics2D.gravity = Vector2.right * defaultGravity;
+                    rotateAnimation.Play("3_DownToLeftRotation");
+                    state = State.vectorLeft;
+                    break;
+                case State.vectorLeft:
+                    playerRigidbody.AddForce(Vector2.left * 4, ForceMode2D.Impulse);
+                    Physics2D.gravity = Vector2.down * defaultGravity;
+                    rotateAnimation.Play("4_LeftToUpRotation");
+                    state = State.vectorUp;
+                    break;
+            }
+        }
+        canRotateAgain = false;
+    }
+
+    /// <summary>
+    /// jump to the opposite wall
+    /// </summary>
+    private void Jump()
+    {
+        playerRigidbody.velocity = Vector2.zero;
+        switch (state)
+        {
+            case State.vectorUp:
+                playerRigidbody.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+                Physics2D.gravity = Vector2.up * defaultGravity;
+                rotateAnimation.Play("10_JumpUp");
+                state = State.vectorDown;
+                break;
+            case State.vectorDown:
+                playerRigidbody.AddForce(Vector2.down * 5, ForceMode2D.Impulse);
+                Physics2D.gravity = Vector2.down * defaultGravity;
+                rotateAnimation.Play("11_JumpDown");
+                state = State.vectorUp;
+                break;
+            case State.vectorRight:
+                playerRigidbody.AddForce(Vector2.right * 5, ForceMode2D.Impulse);
+                Physics2D.gravity = Vector2.right * defaultGravity;
+                rotateAnimation.Play("12_JumpRight");
+                state = State.vectorLeft;
+                break;
+            case State.vectorLeft:
+                playerRigidbody.AddForce(Vector2.left * 5, ForceMode2D.Impulse);
+                Physics2D.gravity = Vector2.left * defaultGravity;
+                rotateAnimation.Play("13_JumpLeft");
+                state = State.vectorRight;
+                break;
+        }
+        canJumpAgain = false;
+        jumpAfterRorating = false;
+    }
+
+
+    /// <summary>
+    /// default values after animation
+    /// </summary>
+    private void SetCanJumpAgain()
+    {
+        canJumpAgain = true;
+        jumpAfterRorating = false;
+    }
+
+    /// <summary>
+    /// if you try to jump while rotaring, the jump will occur in the last frame of the animation
+    /// </summary>
+    private void JumpAfterRorating()
+    {
+        if (jumpAfterRorating == true)
+            Jump();
+        isRotating = false;
     }
 }
